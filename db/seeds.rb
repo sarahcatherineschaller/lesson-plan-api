165de2d45f72e88992daf5500efed5219272b638
Sarah Schaller <sarahcschaller@gmail.com>
2018-11-02 08:59:51 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+lesson_plans = LessonPlan.create(
+	[
+
+		{
+			title: "Solving One-Step Equations",
+			grade_level: 6,
+			subject: "6th Grade Math",
+			total_time: "50 minutes",
+			objective: "Students will be able to solve addition and subtraction equations; Problem solving",
+			materials: "Algebra tiles; Whiteboard; worksheets",
+			summary: "Students will be given algebra tiles to understand how solving addition and subtraction one-step equations work. Use examples such as balance and pairing up negative and positive numbers. Walk through equations using algebra tiles, then move to numbers and symbols on the whiteboard.",
+			other: "Prerequisite: students have an understanding of adding and subtracting negative numbers."
+		},
+		{
+			title: "Factoring Patterns",
+			grade_level: 8,
+			subject: "Algebra I",
+			total_time: "2 hours, split up into multiple classes",
+			objective: "Students will use factoring in solving polynomial equations; Students will solve real world problems by writing and factoring equations",
+			materials: "Textbook, calculators, algebra tiles",
+			summary: "Students will apply order of operations, real number properties, and rules of exponents to simplify algebraic expressions; Students will factor quadratic polynomials of all different patterns",
+			other: nil
+		}
+	])
