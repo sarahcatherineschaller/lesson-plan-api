b1ed89b76ee4cf97a1d4d8096e22ebc6dae5297a
Sarah Schaller <sarahcschaller@gmail.com>
2018-11-03 16:30:04 -0400

    add create, show, update, destroy, params

--- app/controllers/api/v1/lesson_plans_controller.rb
+++ app/controllers/api/v1/lesson_plans_controller.rb
@@ -1,5 +1,34 @@
 module Api::V1
 	class LessonPlansController < ApplicationController 
+		before_action :set_lesson_plan, only: [:show, :update, :destroy]
 
+		def index 
+			@lesson_plans = LessonPlan.all 
+			render json: @lesson_plans 
+		end
+
+		def create 
+			@lesson_plan = LessonPlan.create(lesson_plan_params)
+			render json: @lesson_plan
+		end
+
+		def show 
+			render json: @lesson_plan
+		end
+
+		def update 
+		end
+
+		def destroy 
+		end
+
+		private 
+		def set_lesson_plan 
+			@lesson_plan = LessonPlan.find_by(id: params[:id]) 
+		end
+
+		def lesson_plan_params 
+			params.require(:lesson_plan).permit(:title, :grade_level, :subject, :total_time, :objective, :materials, :summary, :other)
+		end
 	end 
 end
