94fdac061ddd1e4b9fdb0289e9faf7e6d970d717
Sarah Schaller <sarahcschaller@gmail.com>
2018-11-03 09:51:47 -0400

    add lesson plan serializer

--- app/controllers/api/v1/lesson_plans_controller.rb
+++ app/controllers/api/v1/lesson_plans_controller.rb
@@ -1,8 +1,5 @@
 module Api::V1
 	class LessonPlansController < ApplicationController 
-		def index 
-			@lesson_plans = LessonPlan.all 
-			render json: @lesson_plans 
-		end 
+
 	end 
 end
---
+++ app/serializers/lesson_plan_serializer.rb
@@ -0,0 +1,3 @@
+class LessonPlanSerializer < ActiveModel::Serializer 
+	attributes :id, :title, :grade_level, :subject, :total_time, :objective, :materials, :summary, :other 
+end
