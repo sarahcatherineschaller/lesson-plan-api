4c38c1e68eba0a6886da85bae7415b153fc8ffd4
Sarah Schaller <sarahcschaller@gmail.com>
2018-11-06 09:47:04 -0500

    build lesson card

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,4 @@
-lesson_plans = LessonPlan.create(
-	[
-
-		{
-			title: "Solving One-Step Equations",
+LessonPlan.create(title: "Solving One-Step Equations",
 			grade_level: 6,
 			subject: "6th Grade Math",
 			total_time: "50 minutes",
@@ -10,9 +6,9 @@ lesson_plans = LessonPlan.create(
 			materials: "Algebra tiles; Whiteboard; worksheets",
 			summary: "Students will be given algebra tiles to understand how solving addition and subtraction one-step equations work. Use examples such as balance and pairing up negative and positive numbers. Walk through equations using algebra tiles, then move to numbers and symbols on the whiteboard.",
 			other: "Prerequisite: students have an understanding of adding and subtracting negative numbers."
-		},
-		{
-			title: "Factoring Patterns",
+		)
+
+LessonPlan.create(title: "Factoring Patterns",
 			grade_level: 8,
 			subject: "Algebra I",
 			total_time: "2 hours, split up into multiple classes",
@@ -20,5 +16,4 @@ lesson_plans = LessonPlan.create(
 			materials: "Textbook, calculators, algebra tiles",
 			summary: "Students will apply order of operations, real number properties, and rules of exponents to simplify algebraic expressions; Students will factor quadratic polynomials of all different patterns",
 			other: " "
-		}
-	])
+		)
